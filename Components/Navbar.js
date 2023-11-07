739bbeee489998000ad5b30f26392ef7be15e121
vaishak19 <vaishak1911@gmail.com>
2023-11-07 21:19:56 +0530

    first commit

---
+++ Components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <div className="bg-green-600 px-6 py-3  my-4 rounded-xl h-12 flex item-center justify-between">
+        <Link href="/">Envikon Logo</Link>
+        <div className='flex gap-10 text-white font-bold'>
+            <Link href="/Services" >Services</Link>
+            <Link href="/About">About</Link>
+            <Link href="/ContactUs">Contact Us</Link>
+        </div>        
+    </div>
+    </>
+  )
+}
+
+export default Navbar
---
+++ app/page.js
@@ -0,0 +1,16 @@
+"use client"
+import Navbar from '@/Components/Navbar'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <div> 
+      <h1>This is main page</h1>
+    </div>
+    </>
+  )
+}
+
+export default page
